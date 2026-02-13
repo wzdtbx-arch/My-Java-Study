a0d71fe8b370de080d5fe85bf552ea96e95caaec
wzdtbx-arch <wzdtbx@gmail.com>
2026-02-14 02:31:46 +0800

    2026-02-13:练习了数组的练习

---
+++ src/com/itheima/arraydemo/ArrayDeme6.java
@@ -0,0 +1,21 @@
+package com.itheima.arraydemo;
+
+import java.util.Random;
+
+public class ArrayDeme6 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int randonindex = r.nextInt(arr.length);
+            int temp=arr[i];
+             arr[i]=arr[randonindex];
+            arr[randonindex]=temp;
+
+
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
